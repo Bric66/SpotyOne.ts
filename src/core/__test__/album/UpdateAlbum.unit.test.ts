ea93ae098c2ce7d4004b2271c5e006c4a8092b99
benjamin antonioli <benantonioli@outlook.fr>
2022-11-23 10:42:07 +0100

    23/11

--- src/core/__test__/album/UpdateAlbum.unit.test.ts
+++ src/core/__test__/album/UpdateAlbum.unit.test.ts
@@ -1,56 +1,54 @@
-import { UpdateAlbum } from './../../Usecases/album/UpdateAlbum';
-import { Album } from "../../Entities/Album"
-import { InMemoryAlbumRespository } from '../adapters/repositories/InMemoryAlbumRespository';
+import { UpdateAlbum } from "./../../Usecases/album/UpdateAlbum";
+import { Album } from "../../Entities/Album";
+import { InMemoryAlbumRespository } from "../adapters/repositories/InMemoryAlbumRespository";
 
-const db = new Map<string, Album>()
+const db = new Map<string, Album>();
 
+describe("unit - UpdateAlbum", () => {
+  let updateAlbum: UpdateAlbum;
+  let album: Album;
 
-describe('unit - UpdateAlbum', () => {
-    let updateAlbum: UpdateAlbum
-    let album: Album
+  beforeAll(() => {
+    album = new Album({
+      albumId: "12345",
+      userId: "userId",
+      albumTitle: "Album Title",
+      artist: "Album Artist",
+      file: "hhtp://../album",
+      tracks: [
+        {
+          trackId: "132354",
+          trackTitle: "title",
+        },
+        {
+          trackId: "789798",
+          trackTitle: "title",
+        },
+        {
+          trackId: "4654654687",
+          trackTitle: "title",
+        },
+      ],
+      totalDuration: 1547,
+      tracksCount: 3,
+    });
+    const inMemoryAlbumRespository = new InMemoryAlbumRespository(db);
+    updateAlbum = new UpdateAlbum(inMemoryAlbumRespository);
+    db.set(album.props.albumId, album);
+  });
+  it("should update the album", async () => {
+    const result = await updateAlbum.execute({
+      file: "hhtp://../updated",
+      tracks: {
+        trackId: "new id",
+        trackTitle: "new title",
+      },
 
-    beforeAll(() => {
-        album = new Album({
-            albumId: "12345",
-            userId: "userId",
-            albumTitle: "Album Title",
-            artist: "Album Artist",
-            file: "hhtp://../album",
-            tracks: 
-                [  { 
-                         trackId: "132354",
-                         trackTitle: "title"
-                    },
-                    { 
-                        trackId: "789798",
-                        trackTitle: "title"
-                    },
-                    { 
-                        trackId: "4654654687",
-                        trackTitle: "title"
-                    }
-                ],
-            totalDuration: 1547,
-            tracksCount: 3    
-        })
-        const inMemoryAlbumRespository = new InMemoryAlbumRespository(db)
-        updateAlbum = new UpdateAlbum(inMemoryAlbumRespository)
-        db.set(album.props.albumId, album)
-    })
-    it('should update the album', async () => {
-        const result = await updateAlbum.execute({
-            file: "hhtp://../updated",
-            tracks: 
-                { 
-                    trackId: "new id",
-                    trackTitle: "new title"
-                },
-            
-            albumTitle: "updated title",
-            artist: "updated artist",
-            userId: 'userId',
-        })
-        expect(result.props.albumTitle).toEqual("updated title")
-        expect(result.props.tracks).toHaveLength(4)
-    })
-})
+      albumTitle: "updated title",
+      artist: "updated artist",
+      userId: "userId",
+    });
+    expect(result.props.albumTitle).toEqual("updated title");
+    expect(result.props.tracks).toHaveLength(4);
+  });
+});
