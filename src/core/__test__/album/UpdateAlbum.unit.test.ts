f3a5032c999dcd8f460a0247328dcbed0ae98b0f
Cédric Herouard <115704191+Bric66@users.noreply.github.com>
2022-11-24 17:04:20 +0100

    integration ced

--- src/core/__test__/album/UpdateAlbum.unit.test.ts
+++ src/core/__test__/album/UpdateAlbum.unit.test.ts
@@ -1,54 +1,56 @@
-import { UpdateAlbum } from "./../../Usecases/album/UpdateAlbum";
-import { Album } from "../../Entities/Album";
-import { InMemoryAlbumRespository } from "../adapters/repositories/InMemoryAlbumRespository";
+import {UpdateAlbum} from "./../../Usecases/album/UpdateAlbum";
+import {Album} from "../../Entities/Album";
+import {InMemoryAlbumRespository} from "../adapters/repositories/InMemoryAlbumRespository";
 
 const db = new Map<string, Album>();
 
 describe("unit - UpdateAlbum", () => {
-  let updateAlbum: UpdateAlbum;
-  let album: Album;
+    let updateAlbum: UpdateAlbum;
+    let album: Album;
 
-  beforeAll(() => {
-    album = new Album({
-      albumId: "12345",
-      userId: "userId",
-      albumTitle: "Album Title",
-      artist: "Album Artist",
-      file: "hhtp://../album",
-      tracks: [
-        {
-          trackId: "132354",
-          trackTitle: "title",
-        },
-        {
-          trackId: "789798",
-          trackTitle: "title",
-        },
-        {
-          trackId: "4654654687",
-          trackTitle: "title",
-        },
-      ],
-      totalDuration: 1547,
-      tracksCount: 3,
+    beforeAll(() => {
+        album = new Album({
+            albumId: "12345",
+            userId: "userId",
+            albumTitle: "Album Title",
+            artist: "Album Artist",
+            file: "hhtp://../album",
+            tracks: [
+                {
+                    trackId: "132354",
+                    trackTitle: "title",
+                },
+                {
+                    trackId: "789798",
+                    trackTitle: "title",
+                },
+                {
+                    trackId: "4654654687",
+                    trackTitle: "title",
+                },
+            ],
+            created: new Date(),
+            updated: null,
+        });
+        const inMemoryAlbumRespository = new InMemoryAlbumRespository(db);
+        updateAlbum = new UpdateAlbum(inMemoryAlbumRespository);
+        db.set(album.props.albumId, album);
     });
-    const inMemoryAlbumRespository = new InMemoryAlbumRespository(db);
-    updateAlbum = new UpdateAlbum(inMemoryAlbumRespository);
-    db.set(album.props.albumId, album);
-  });
-  it("should update the album", async () => {
-    const result = await updateAlbum.execute({
-      file: "hhtp://../updated",
-      tracks: {
-        trackId: "new id",
-        trackTitle: "new title",
-      },
+    it("should update the album", async () => {
+        const result = await updateAlbum.execute({
+            file: "hhtp://../updated",
+            tracks: {
+                trackId: "new id",
+                trackTitle: "new title",
+            },
 
-      albumTitle: "updated title",
-      artist: "updated artist",
-      userId: "userId",
+            albumTitle: "updated title",
+            artist: "updated artist",
+            userId: "userId",
+            updated: new Date(),
+        });
+        expect(result.props.albumTitle).toEqual("updated title");
+        expect(result.props.tracks).toHaveLength(4);
+        expect(result.props.updated).toBeTruthy();
     });
-    expect(result.props.albumTitle).toEqual("updated title");
-    expect(result.props.tracks).toHaveLength(4);
-  });
 });
